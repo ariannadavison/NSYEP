39480ee9c89755ae33d42af0b3aa39c6aabc2c74
honeyai <contact.jessicasanchez@gmail.com>
2021-04-16 21:25:40 -0400

    laid out the front page and in a comment laid out the other routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import './App.css';
 
+//*Structure for Region Page:
+// <logoHero />
+// navbar
+// <RegionBanners />
+// copy
+// <YearlyInfo />
+// <PhotoBubbles />
+// <Footer />
+
+//*Structure for Individual Story Page
+// <logoHero />
+// navbar
+// media
+// copy or interview (2019 - 2017)
+// <Footer />
+
+//*Structure for Contact Page
+// <logoHero />
+// navbar
+// <MottoBanner />
+// <Contact />
+// <Form /> beyond mvp
+// <Footer />
+
+
+
 function App() {
   return (
     <div className="App">
+      <LogoHero />
+      {/* NavBar */}
+      <MottoBanner />
+      {/* COPY */}
+      <PageDivider />
+      {/* MAP */}
+      <Footer />
     </div>
   );
 }
