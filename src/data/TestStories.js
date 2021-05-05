7f8a093826fe3cad0859d679a07f5b81cc87699b
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2021-05-05 13:46:11 -0400

    adding a data json

---
+++ src/data/TestStories.js
@@ -0,0 +1,13 @@
+export const storyBank = {
+  Region: "Capital",
+  Stories: {
+    img: "",
+    media: {
+      name: "Sandra Bullock",
+      age: "40",
+      
+      interview: "",
+
+    }
+  },
+};
