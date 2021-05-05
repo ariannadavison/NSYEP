8f1afe52be7feaf1a2714383ce91116d23e6da2d
honeyai <contact.jessicasanchez@gmail.com>
2021-05-05 13:52:06 -0400

    yearly info changes added?

--- src/components/YearlyInfo/YearlyInfo.js
+++ src/components/YearlyInfo/YearlyInfo.js
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
-import PhotoBubbles from '../PhotoBubbles/PhotoBubbles';
-import YearlyBanner from './YearlyBanner';
-import YearStats from './YearStats';
+import React, { useState } from "react";
+import PhotoBubbles from "../PhotoBubbles/PhotoBubbles";
+import YearlyBanner from "./YearlyBanner";
+import YearStats from "./YearStats";
 
-function YearlyInfo({regionName}) {
-  const [bannerView, setBannerView] = useState('year-2020');
+function YearlyInfo({ regionName }) {
+  const [bannerView, setBannerView] = useState("year-2020");
 
   function changeBannerView(e) {
     const name = e.target.getAttribute("name");
     setBannerView(name);
-    return <PhotoBubbles year={name} regionName={regionName}/>
+    return <PhotoBubbles year={name} regionName={regionName} />;
   }
 
   function getYearlyView() {
-    switch(bannerView) {
-      case 'year-2020':
-        return <YearStats year="2020" />
-      case 'year-2019':
-        return <YearStats year="2019" />
-      case 'year-2018':
-        return <YearStats year="2018" />
-      case 'year-2017':
-        return <YearStats year="2017" />
+    switch (bannerView) {
+      case "year-2020":
+        return <YearStats year="2020" />;
+      case "year-2019":
+        return <YearStats year="2019" />;
+      case "year-2018":
+        return <YearStats year="2018" />;
+      case "year-2017":
+        return <YearStats year="2017" />;
       default:
-        return <YearStats year="2020" />
+        return <YearStats year="2020" />;
     }
   }
 
-  return(
+  return (
     <div>
-      <YearlyBanner changeBannerView={ changeBannerView } />
-      { getYearlyView() }
+      <YearlyBanner changeBannerView={changeBannerView} />
+      {getYearlyView()}
     </div>
-  )
+  );
 }
 
 export default YearlyInfo;
