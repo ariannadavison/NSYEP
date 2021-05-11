8c4d130f4acbe2cd0c12d6cb92f4890d309b546b
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-05-11 18:14:52 -0400

    updated style for page divider to be more convenient

--- src/components/PageDivider/PageDivider.js
+++ src/components/PageDivider/PageDivider.js
@@ -4,21 +4,11 @@ import "./pageDivider.css";
 const PageDivider = () => {
   return (
     <div className="pageDivider__container">
-      <div className="pageDivider__right">
-        <div className="pageDivider__rect" />
-        <div className="pageDivider__shapes">
-          <div className="pageDivider__triangleTopLeft" />
-          <div className="pageDivider__triangleBottomLeft" />
-        </div>
+      <div className="pageDivider__left"></div>
+      <div className="pageDivider__text">
+        <span>Explore the stories in your area</span>
       </div>
-      <div className="pageDivider__text"> Explore the stories in your area </div>
-      <div className="pageDivider__left">
-        <div className="pageDivider__shapes">
-          <div className="pageDivider__triangleTopRight" />
-          <div className="pageDivider__triangleBottomRight" />
-        </div>
-        <div className="pageDivider__rect" />
-      </div>
+      <div className="pageDivider__right"></div>
     </div>
   );
 };
