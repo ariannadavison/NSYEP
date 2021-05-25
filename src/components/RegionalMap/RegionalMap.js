8a3c720f0d5563c0d35c86023d8d14f45d533968
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-05-25 18:23:42 -0400

    colors for regional map

--- src/components/RegionalMap/RegionalMap.js
+++ src/components/RegionalMap/RegionalMap.js
@@ -25,16 +25,16 @@ const RegionalMap = () => {
     <div className="map">
       <div className="map__regionNames">
         <ul>
-          <li className="map__regionName"><Link to="/western" className="map__regionName map__regionName--turq">Western</Link></li>
+          <li className="map__regionName"><Link to="/western" className="map__regionName--turq">Western</Link></li>
           <li className="map__regionName"><Link to="/finger-lakes" className="map__regionName--pink">Finger Lakes</Link></li>
-          <li className="map__regionName"><Link to="/central" className="map__regionName--blue">Central</Link></li>
+          <li className="map__regionName"><Link to="/central" className="map__regionName--ind">Central</Link></li>
           <li className="map__regionName"><Link to="/southern" className="map__regionName--lime">Southern</Link></li>
           <li className="map__regionName"><Link to="/north" className="map__regionName--gold">North</Link></li>
           <li className="map__regionName"><Link to="/mohawk-valley" className="map__regionName--red">Mohawk Valley</Link></li>
           <li className="map__regionName"><Link to="/capital" className="map__regionName--mint">Capital</Link></li>
-          <li className="map__regionName"><Link to="/hudson-valley" className="map__regionName--purp">Hudson Valley</Link></li>
+          <li className="map__regionName"><Link to="/hudson-valley" className="map__regionName--blue">Hudson Valley</Link></li>
           <li className="map__regionName"><Link to="/new-york-city" className="map__regionName--sun">New York City</Link></li>
-          <li className="map__regionName"><Link to="/long-island" className="map__regionName--ind">Long Island</Link></li>
+          <li className="map__regionName"><Link to="/long-island" className="map__regionName--purp">Long Island</Link></li>
         </ul>
       </div>
 
