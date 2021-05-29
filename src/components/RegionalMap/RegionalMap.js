56d453054413f8715ad3b3c85806380e18d9710d
honeyes <contact.jessicasanchez@gmail.com>
2021-05-28 23:06:51 -0400

    map now routes to the region and highlights the region rather than the individual counties

--- src/components/RegionalMap/RegionalMap.js
+++ src/components/RegionalMap/RegionalMap.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import MapV2 from "../MapV2/MapV2";
-import './regionalMap.css';
+import "./regionalMap.css";
 
 const RegionalMap = () => {
-
   useEffect(() => {
     function callback() {
       if (
@@ -26,16 +25,56 @@ const RegionalMap = () => {
     <div className="mappy">
       <div className="map__regionNames">
         <ul>
-          <li className="map__regionName"><Link to="/western" id="map__regionNameLinks">Western</Link></li>
-          <li className="map__regionName"><Link to="/finger-lakes" id="map__regionNameLinks">Finger Lakes</Link></li>
-          <li className="map__regionName"><Link to="/central" id="map__regionNameLinks">Central</Link></li>
-          <li className="map__regionName"><Link to="/southern" id="map__regionNameLinks">Southern</Link></li>
-          <li className="map__regionName"><Link to="/north" id="map__regionNameLinks">North</Link></li>
-          <li className="map__regionName"><Link to="/mohawk-valley" id="map__regionNameLinks">Mohawk Valley</Link></li>
-          <li className="map__regionName"><Link to="/capital" id="map__regionNameLinks">Capital</Link></li>
-          <li className="map__regionName"><Link to="/hudson-valley" id="map__regionNameLinks">Hudson Valley</Link></li>
-          <li className="map__regionName"><Link to="/new-york-city" id="map__regionNameLinks">New York City</Link></li>
-          <li className="map__regionName"><Link to="/long-island" id="map__regionNameLinks">Long Island</Link></li>
+          <li className="map__regionName">
+            <Link to="/western" id="map__regionNameLinks">
+              Western
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/finger-lakes" id="map__regionNameLinks">
+              Finger Lakes
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/central" id="map__regionNameLinks">
+              Central
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/southern" id="map__regionNameLinks">
+              Southern
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/north" id="map__regionNameLinks">
+              North
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/mohawk-valley" id="map__regionNameLinks">
+              Mohawk Valley
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/capital" id="map__regionNameLinks">
+              Capital
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/hudson-valley" id="map__regionNameLinks">
+              Hudson Valley
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/new-york-city" id="map__regionNameLinks">
+              New York City
+            </Link>
+          </li>
+          <li className="map__regionName">
+            <Link to="/long-island" id="map__regionNameLinks">
+              Long Island
+            </Link>
+          </li>
         </ul>
       </div>
       <MapV2 />
