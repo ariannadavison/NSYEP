f0caff57a4626ed365b5a9a2aac191ac9b74658f
honeyes <contact.jessicasanchez@gmail.com>
2021-06-14 14:57:02 -0400

    changes to responsive elements in footer and regional map

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./variables.css";
 import "./index.css";
-import "./honeyaiVariables.css"
+import "/mnt/c/Users/jsami/desktop/NSYEP/src/responsiveFont.css";
+import "./honeyaiVariables.css";
 import App from "./App";
 
 ReactDOM.render(
