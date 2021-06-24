75d1cfdf65be5c08e1eb8f3d000d72be6e9cfad1
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-06-24 17:37:58 -0400

    two versions of the footer - change the stylesheet between footer.css and footer2.css to view

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,23 @@
 import React from "react";
-import "./footer.css";
+import "./footer2.css";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__columns">
-        <div>
-          <p>Basic</p>
-          1725 Slough Avenue Scranton, PA 1850
+        <div className="footer__info">
+          <p>NYATEP</p>
+          <p>P.O. Box 648</p>
+          <p>Albany, New York 12201</p>
+          <p>518-728-9057</p>
         </div>
-        <div>Contact</div>
-        <div>Category One</div>
+        <div className="footer__social">
+          <div className="twitter social-placeholder"></div>
+          <div className="linkedin social-placeholder"></div>
+          <div className="facebook social-placeholder"></div>
+          <div className="instagram social-placeholder"></div>
+        </div>
       </div>
-      <div>Copyright &copy; All Rights Reserved</div>
-      <div className="footer__social"></div>
     </div>
   )
 }
