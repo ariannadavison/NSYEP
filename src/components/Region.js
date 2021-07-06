7469beba45050b89ee1878a05a8a6f8ff76be192
honeyes <contact.jessicasanchez@gmail.com>
2021-07-06 13:42:09 -0400

    getting the images into the template.

--- src/components/Region.js
+++ src/components/Region.js
@@ -1,21 +1,321 @@
 import RegionBanner from "./RegionBanners/RegionBanner";
-import YearlyInfo from "./YearlyInfo/YearlyInfo";
+import { regions } from "../data/Regions";
+// import YearlyInfo from "./YearlyInfo/YearlyInfo";
 
-export default function Region (props) {
+export default function Region(props) {
   console.log(props.match.params.regionName);
   let name = props.match.params.regionName;
   // the name comes through as the url, so we must name-ify it.
-  name = name.replace(/-/g,' ');
+  name = name.replace(/-/g, " ");
   name = name.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase());
+  console.log(name, "name");
+  //gotta locate the right object that's holding the region name:
+
+  let region = regions.filter((r) => r.region === name);
+  let content = region[0].content;
+  console.log(content, "filtered region");
+
+  console.log("Does name === Capital", name === "Capital");
+
+  let images;
+
+  const importImages = (r) => r.keys().map(r);
+
+  switch (name) {
+    case "Capital":
+      images = importImages(
+        require.context(`./Images/Capital`, false, /\.(png|jpe?g|svg)$/)
+      );
+      console.log(images, "images");
+      break;
+    // case "Finger Lakes":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/FingerLakes/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //   break;
+    // case "Central":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Central/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Central/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Central/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "Southern":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Southern/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Southern/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Southern/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "North":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(`../Images/North/2020`, false, /\.(png|jpe?g|svg)$/)
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(`../Images/North/2019`, false, /\.(png|jpe?g|svg)$/)
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(`../Images/North/2018`, false, /\.(png|jpe?g|svg)$/)
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "Mohawk Valley":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/MohawkValley/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/MohawkValley/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/MohawkValley/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "Hudson Valley":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/HudsonValley/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/HudsonValley/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/HudsonValley/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "New York City":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/NewYorkCity/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/NewYorkCity/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/NewYorkCity/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "Long Island":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/LongIsland/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/LongIsland/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/LongIsland/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    // case "Western":
+    //   switch (year) {
+    //     case "2020":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Western/2020`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2019":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Western/2019`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     case "2018":
+    //       images = importImages(
+    //         require.context(
+    //           `../Images/Western/2018`,
+    //           false,
+    //           /\.(png|jpe?g|svg)$/
+    //         )
+    //       );
+    //       break;
+    //     default:
+    //       console.log("The year's haven't been seen.");
+    //   }
+    //   break;
+    default:
+      console.log("it's not doing what you think it's doing");
+  }
 
   return (
     <div>
-      <RegionBanner region={name}/>
+      <RegionBanner region={name} />
       <div className="margins">
-        i am a region.<br />
+        i am a region.
+        <br />
         specifically, the region known as {name}.
       </div>
-        <YearlyInfo regionName={name} />
+      <div className="region__content">
+        {/* probably will be a map as according to the number of things to put into the page */}
+        {content.map((c, key) => {
+          //cylce thru names of images to check if it matches our wanted img
+          // let image = images[c.image].default;
+          return (
+            <div key={key}>
+              {c.title}
+              {/* <img src={require(image)} /> */}
+              {c.copy}
+            </div>
+          );
+        })}
+      </div>
+      {/* <YearlyInfo regionName={name} /> */}
     </div>
-  )
+  );
 }
--- src/data/Regions.js
+++ src/data/Regions.js
@@ -32,7 +32,38 @@ export const regions = [
   {
     region: "Capital",
     regionID: "07",
-    regionURL: "capital"
+    regionURL: "capital",
+    content: [
+      {
+        title: "City of Albany Department of Recreation - Youth and Workforce Services",
+        image: "./components/Images/Capital/2020/Port of albany 1.jpg",
+        copy: "Young adults deserve to reach their full potential and in order for them to achieve it, someone must give them access to different career opportunities that exist around them. In 2020 our visit to the Port of Albany gave them just that. An opportunity to learn about renewable energy, and why it is so important to create and live in a clean environment. During the visit our youth learned about how the windmills work and how large the wingspan of a turbine is. Our youth were engaged and asked questions to the Port of Albany staff about the environmental impact the facility will have on the surrounding communities, if the jobs created will be accessible to the local residents and if so, how many jobs are currently available? At the culmination of the day the youth and the staff from the City of Albany Department of Recreation - Youth and Workforce Services came together as a team to create a board of all the key information gathered during the visit to be presented to the Port of Albany staff. Our youth were provided with lots of great feedback about the content of their presentations and their public speaking skills. They left feeling enthusiastic and empowered by a day filled with new experiences and a new network of professionals they can continue to connect with along their employment journey."
+      },
+      {
+        title: "Schenectady County Job Training Agency",
+        image: "../components/Images/Capital/2020/Screen Shot 2021-06-14 at 1.07.07 AM.png",
+        copy: "During Nhyrobie’s first week of work at CVS, she was very quiet and timid. She had a difficult time understanding the tasks that she was required to complete. However, by her second or third week of work, she had improved immensely. She completed her tasks flawlessly, became much chattier and open during our conversations, and began to develop relationships with her coworkers and supervisors. Nhyrobie was always on time for work and never missed a day. During the hours that she was there, she worked diligently and efficiently to complete all of her tasks. At the end of the summer program, Nhyrobie’s supervisor suggested that she apply to CVS directly so that she could continue to be a part of their team. Nhyrobie completed the lengthy application process and was hired on to continue working as a CVS employee. Nhyrobie grew tremendously throughout the summer program and is well on her way to learning the skills needed for future self-sustainability.",
+      },
+      {
+        title: "Saratoga County Dept. of Employment & Training",
+        image: "../components/Images/Capital/2020/Julia_Bacon_SYEP_2020 - Katherine Raymond.jpg",
+        copy: `Julia started going to The Salvation Army earlier in the year as a program participant in her High School After School program and volunteered for their Elementary After School Program. She was very shy but quickly became comfortable because she loved having a role helping with the kids. 
+
+        The Salvation Army encouraged her to apply for SYEP because they felt it would help with her growth and provide her with a great experience. She successfully applied for SYEP 2020. Her primary job duty was to assist with summer children and food programming, but she helped overall wherever it was needed as part of the team. 
+        
+        She was excited to be involved on an official level and although things were quite different due to COVID, she was extremely flexible and played a crucial role in filling in gaps in programming. Julia took responsibility for the summer feeding program for Galway schools (a local school district). She learned how to manage the program quickly and within a week was making sure everything was executed efficiently and properly by herself. She even successfully recruited volunteers to assist with the program! 
+        
+        This SYEP Shining Star was described as “blossoming” and having “made major strides considering how shy she was” and the worksite is excited to see what her future will hold. 
+        
+        Julia stated the following about her SYEP 2020 experience:
+        “I really enjoyed the summer work program. I learned there are many people in our country that are less fortunate than me. I was glad to be a part of a team that could help feed them and make them smile. I learned about clocking hours and managing money and also things about banking, ID requirements and my social security number. I was even able to help my mom a little bit with our bills and that made me feel really good. I am excited about working with the summer youth program again next year. Thank you so much for this experience and opportunity!”`,
+      },
+      {
+        title: "Warren County Employment & Training",
+        image: "../components/Images/Capital/2020/John.jpg",
+        copy: "Requesting access from evelyn",
+      },
+    ]
   },
   {
     region: "Hudson Valley",
