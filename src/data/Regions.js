cdb633dd5a631877477240b114464c09ac080913
honeyes <contact.jessicasanchez@gmail.com>
2021-07-06 15:11:54 -0400

    got the pictures to appear

--- src/data/Regions.js
+++ src/data/Regions.js
@@ -36,17 +36,17 @@ export const regions = [
     content: [
       {
         title: "City of Albany Department of Recreation - Youth and Workforce Services",
-        image: "./components/Images/Capital/2020/Port of albany 1.jpg",
+        image: "Port of albany 1",
         copy: "Young adults deserve to reach their full potential and in order for them to achieve it, someone must give them access to different career opportunities that exist around them. In 2020 our visit to the Port of Albany gave them just that. An opportunity to learn about renewable energy, and why it is so important to create and live in a clean environment. During the visit our youth learned about how the windmills work and how large the wingspan of a turbine is. Our youth were engaged and asked questions to the Port of Albany staff about the environmental impact the facility will have on the surrounding communities, if the jobs created will be accessible to the local residents and if so, how many jobs are currently available? At the culmination of the day the youth and the staff from the City of Albany Department of Recreation - Youth and Workforce Services came together as a team to create a board of all the key information gathered during the visit to be presented to the Port of Albany staff. Our youth were provided with lots of great feedback about the content of their presentations and their public speaking skills. They left feeling enthusiastic and empowered by a day filled with new experiences and a new network of professionals they can continue to connect with along their employment journey."
       },
       {
         title: "Schenectady County Job Training Agency",
-        image: "../components/Images/Capital/2020/Screen Shot 2021-06-14 at 1.07.07 AM.png",
+        image: "Nhyrobie",
         copy: "During Nhyrobie’s first week of work at CVS, she was very quiet and timid. She had a difficult time understanding the tasks that she was required to complete. However, by her second or third week of work, she had improved immensely. She completed her tasks flawlessly, became much chattier and open during our conversations, and began to develop relationships with her coworkers and supervisors. Nhyrobie was always on time for work and never missed a day. During the hours that she was there, she worked diligently and efficiently to complete all of her tasks. At the end of the summer program, Nhyrobie’s supervisor suggested that she apply to CVS directly so that she could continue to be a part of their team. Nhyrobie completed the lengthy application process and was hired on to continue working as a CVS employee. Nhyrobie grew tremendously throughout the summer program and is well on her way to learning the skills needed for future self-sustainability.",
       },
       {
         title: "Saratoga County Dept. of Employment & Training",
-        image: "../components/Images/Capital/2020/Julia_Bacon_SYEP_2020 - Katherine Raymond.jpg",
+        image: "Julia_Bacon_SYEP_2020 - Katherine Raymond",
         copy: `Julia started going to The Salvation Army earlier in the year as a program participant in her High School After School program and volunteered for their Elementary After School Program. She was very shy but quickly became comfortable because she loved having a role helping with the kids. 
 
         The Salvation Army encouraged her to apply for SYEP because they felt it would help with her growth and provide her with a great experience. She successfully applied for SYEP 2020. Her primary job duty was to assist with summer children and food programming, but she helped overall wherever it was needed as part of the team. 
@@ -60,7 +60,7 @@ export const regions = [
       },
       {
         title: "Warren County Employment & Training",
-        image: "../components/Images/Capital/2020/John.jpg",
+        image: "John",
         copy: "Requesting access from evelyn",
       },
     ]
