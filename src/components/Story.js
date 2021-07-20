ab626f3fb19732b4111f6f1c40a4a9263c7ab21b
honeyes <contact.jessicasanchez@gmail.com>
2021-07-20 19:35:22 -0400

    deleted obsolete components

--- src/components/Story.js
+++
@@ -1,9 +0,0 @@
-export default function Story(props) {
-  return (
-    <div>
-      this is where the person's story goes.<br />
-
-      this is the individual story of the person known as {props.match.params.storyName} from the region called {props.match.params.regionName}.
-    </div>
-  )
-}
