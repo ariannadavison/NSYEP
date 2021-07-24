d3c6e4408f07ab7a94be7eb03648700b34ecca6e
honeyes <contact.jessicasanchez@gmail.com>
2021-07-24 14:47:42 -0400

    redo of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,24 +28,6 @@ const Navbar = (props) => {
           <NavLink href="/" className="navbar__link">Home</NavLink>
         </NavItem>
           <NavLink href="/regions" className="navbar__link">Find Your Region</NavLink>
-        {/* <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle className="navbar__link" nav caret> */}
-            
-          {/* </DropdownToggle> */}
-          {/* <DropdownMenu> */}
-            {/* <DropdownItem href="#">Find Your Region */}
-            {/* href points to interactive map */}
-            {/* </DropdownItem> */}
-
-            {/* {regions.map(r => <Link to={`/${r.regionURL}`} key={r.regionID}><DropdownItem>{r.region}</DropdownItem></Link>)} */}
-
-          {/* </DropdownMenu> */}
-        {/* </Dropdown> */}
-        {/* <NavItem>
-          <NavLink>
-            <Link to="/contact" className="navbar__link">Get in Touch with NSYEP Sites</Link>
-          </NavLink>
-        </NavItem> */}
       </Nav>
     </div>
   );
