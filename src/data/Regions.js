a903f40012a8136a0bbef990e2080a523ee38a18
honeyes <contact.jessicasanchez@gmail.com>
2021-07-24 16:08:10 -0400

    added Content to Regions.js

--- src/data/Regions.js
+++ src/data/Regions.js
@@ -4,19 +4,83 @@ export const regions = [
     region: "Capital",
     regionID: "07",
     regionURL: "capital",
-    counties: ['Albany', 'Clinton', 'Columbia', 'Greene', 'Rensselaer', 'Saratoga', 'Schedectady', 'Warren', 'Washington']
+    counties: ['Albany', 'Clinton', 'Columbia', 'Greene', 'Rensselaer', 'Saratoga', 'Schedectady', 'Warren', 'Washington'],
+    content: [
+      {
+        title: "City of Albany Department of Recreation - Youth and Workforce Services",
+        image: "Port of albany 1",
+        copy: "Young adults deserve to reach their full potential and in order for them to achieve it, someone must give them access to different career opportunities that exist around them. In 2020 our visit to the Port of Albany gave them just that. An opportunity to learn about renewable energy, and why it is so important to create and live in a clean environment. During the visit our youth learned about how the windmills work and how large the wingspan of a turbine is. Our youth were engaged and asked questions to the Port of Albany staff about the environmental impact the facility will have on the surrounding communities, if the jobs created will be accessible to the local residents and if so, how many jobs are currently available? At the culmination of the day the youth and the staff from the City of Albany Department of Recreation - Youth and Workforce Services came together as a team to create a board of all the key information gathered during the visit to be presented to the Port of Albany staff. Our youth were provided with lots of great feedback about the content of their presentations and their public speaking skills. They left feeling enthusiastic and empowered by a day filled with new experiences and a new network of professionals they can continue to connect with along their employment journey."
+      },
+      {
+        title: "Schenectady County Job Training Agency",
+        image: "Nhyrobie",
+        copy: "During Nhyrobie’s first week of work at CVS, she was very quiet and timid. She had a difficult time understanding the tasks that she was required to complete. However, by her second or third week of work, she had improved immensely. She completed her tasks flawlessly, became much chattier and open during our conversations, and began to develop relationships with her coworkers and supervisors. Nhyrobie was always on time for work and never missed a day. During the hours that she was there, she worked diligently and efficiently to complete all of her tasks. At the end of the summer program, Nhyrobie’s supervisor suggested that she apply to CVS directly so that she could continue to be a part of their team. Nhyrobie completed the lengthy application process and was hired on to continue working as a CVS employee. Nhyrobie grew tremendously throughout the summer program and is well on her way to learning the skills needed for future self-sustainability.",
+      },
+      {
+        title: "Saratoga County Dept. of Employment & Training",
+        image: "Julia_Bacon_SYEP_2020 - Katherine Raymond",
+        copy: `Julia started going to The Salvation Army earlier in the year as a program participant in her High School After School program and volunteered for their Elementary After School Program. She was very shy but quickly became comfortable because she loved having a role helping with the kids. 
+
+        The Salvation Army encouraged her to apply for SYEP because they felt it would help with her growth and provide her with a great experience. She successfully applied for SYEP 2020. Her primary job duty was to assist with summer children and food programming, but she helped overall wherever it was needed as part of the team. 
+        
+        She was excited to be involved on an official level and although things were quite different due to COVID, she was extremely flexible and played a crucial role in filling in gaps in programming. Julia took responsibility for the summer feeding program for Galway schools (a local school district). She learned how to manage the program quickly and within a week was making sure everything was executed efficiently and properly by herself. She even successfully recruited volunteers to assist with the program! 
+        
+        This SYEP Shining Star was described as “blossoming” and having “made major strides considering how shy she was” and the worksite is excited to see what her future will hold. 
+        
+        Julia stated the following about her SYEP 2020 experience:
+        “I really enjoyed the summer work program. I learned there are many people in our country that are less fortunate than me. I was glad to be a part of a team that could help feed them and make them smile. I learned about clocking hours and managing money and also things about banking, ID requirements and my social security number. I was even able to help my mom a little bit with our bills and that made me feel really good. I am excited about working with the summer youth program again next year. Thank you so much for this experience and opportunity!”`,
+      },
+      {
+        title: "Warren County Employment & Training",
+        image: "John",
+        copy: "Requesting access from evelyn",
+      },
+    ]
   },
   {
     region: "Central",
     regionID: "03",
     regionURL: "central",
-    counties: ['Cayuga', 'Certland', 'Madison', 'Onondega', 'Oswego']
+    counties: ['Cayuga', 'Certland', 'Madison', 'Onondega', 'Oswego'],
+    content: [
+      {
+        title: "Cayuga Cortland Workforce Development Board",
+        image: "Malcolm Wilson_Central New York",
+        copy: "Malcolm Wilson, 15, of Cortland, NY was one of four participants in Cortland County SYEP’s first ever remote work readiness and career exploration training. Because of the lack of worksites for SYEP participants, Special Programs Counselor Jackie Pittsley, designed and delivered the comprehensive content via video chat and independent assignments. Malcolm, an honor student at Cortland Jr/Sr High School, successfully completed the training and offered valuable feedback on the lessons that can be used for future training.",
+      },
+      {
+        title: "",
+        image: "Melynna Vasquez_Central New York",
+        copy: "Melynna Vasquez, 15, of McGraw, NY worked with SYEP at Lime Hollow Nature Center. Melynna wanted to be at a worksite where she could be active. According to her supervisor, Melynna was a leader in the crew and set an example of what hard work and dedication look like. Melynna had perfect attendance during her placement and was part of a group of youth that was recognized locally for their contribution to major improvements at the non-profit.",
+      },
+      {
+        title: "",
+        image: "Sienna Haskins_Central New York",
+        copy: "Sienna Haskins, 17, of Homer, NY returned for her third summer as a SYEP participant and her second summer at Center for Arts. Sienna’s duties included clerical work, cleaning and organizing, ticket sales, and event preparation for the Center’s socially distanced parking lot performance series. Because of her love for the venue and the staff, Sienna volunteers at the Center throughout the year.",
+      },
+    ],
   },
   {
     region: "Finger Lakes",
     regionID: "02",
     regionURL: "finger-lakes",
-    counties: ['Genesee', 'Livingston', 'Monroe', 'Ontario', 'Orleane', 'Seneca', 'Wayne', 'Wyoming', 'Yates']
+    counties: ['Genesee', 'Livingston', 'Monroe', 'Ontario', 'Orleane', 'Seneca', 'Wayne', 'Wyoming', 'Yates'],content: [
+      {
+        title: "Livingston County Workforce Development",
+        image: "tainairis goodwill - Mary Guldenschuh",
+        copy: "This year marked the fourth summer for 17 year old Tainairis that she participated in SYEP.  At her employment interview she asked if she could get help this summer to find an after school job for the fall.  Bearing this in mind, she was matched with Goodwill of the Finger Lakes. Early on, Tainairis identified an interest in being a cashier and began inquiring about open positions.  Meanwhile, her Employment Counselor, Kayla,  assisted by updating her resume and master application and practicing interview questions.  When the time came to interview, Tainairis was permitted to have Kayla in the room for support.  Tainairis finished her commitment to working for us in the summer program and was directly hired on by Goodwill!  When asked what advice she would pass on to new recruits, Tainairis said “be on time, do your work, don’t miss days, and don’t be scared to ask questions.”  It has been a gratifying experience to bear witness to 14 year old Tainairis grow in confidence and maturity over the past 4 years.",
+      },
+      {
+        title: "OrleansCounty Job Development",
+        image: "OrleansCounty Job Development",
+        copy: "Although 2020 brought upon hardships due to COVID-19, the youth were still able to gain valuable work experience. This year brought out new methods for virtual training and employment as well as new outdoor employment opportunities that have not been offered in the past. Adam Kremblas who was placed at Orleans County Department of Public Works shares that he recommends this program to all youth in the future. “I learned several different things about grounds keeping and general repair through the county. I really loved working there and maybe someday in the future I can work there again, but everything I learned throughout my employment in general will help me in the future.”",
+      },
+      {
+        title: "Yates County Workforce Development",
+        image: "G _Yates County Workforce Development ",
+        copy: `G is an 18 year old Job Corps student, who was sent home in March due to the pandemic. She was just months from completing her education and training at Job Corps when she came to our office. G had participated in SYEP in past summers and her mother knew about the WIOA Youth program so she asked if we could assist G in completing the steps she needed. G applied for the Summer Youth Employment Program in early July and completed enrollment in the WIOA Youth program in August. G needed a work experience that could lead to employment to help her become self-sufficient. We offered her a work placement at Subway. She began working approximately 30 hours per week and she started working on her Job Corps requirements. Along the way we learned that she was living in a camper on the family property which would only be available until the end of October. Not only did she need employment but she also needed stable housing. Typically, SYEP workers at Subway, work in a support position doing cleaning, dishes and general support of the other staff. G quickly settled in and became an asset. She was cross trained for a "Sandwich Artist" position and at the end of SYEP was offered a position with the company. G's hard work, great customer service skills and strong work ethic paid off as she was able to move out of the camper before the cold weather started and into an apartment she shares with others. She completed her Job Corps requirements and is finding her way to independence. Although her training is in Industrial Painting, G recognizes that for now, this job is a good fit allowing her time to complete driving lessons and get her license so that she can move onto bigger things.`,
+      },
+    ],
   },
   {
     region: "Hudson Valley",
@@ -28,7 +92,23 @@ export const regions = [
     region: "Long Island",
     regionID: "10",
     regionURL: "long-island",
-    counties: ['Nassau', 'Suffolk']
+    counties: ['Nassau', 'Suffolk'],
+    content: [
+      {
+        title: "Suffolk County Department of Labor, Licensing, and Consumer Affairs",
+        image: "2020",
+        copy: "The Suffolk County Department of Labor, Licensing and Consumer Affairs was able to successfully operate our 2020 Summer Youth Employment Program despite experiencing obstacles from the COVID-19 pandemic. One of our young adults, Eddie, worked at Housing Help Inc. in Greenlawn as a Food Pantry Aide. He helped with food distribution, stocking shelves, and community outreach. In appreciation for all the support and amazing work experience he received Eddie shared the following message with Ms. Pilar Moya-Mancera, the Executive Director of Housing Help Inc about his experience “ You taught me how to become a better man, you taught me how to display good qualities, you taught me about self discipline and you also taught me about working together as a community. It was very hard for me to look for a job during and before the COVID-19 pandemic. You have opened many windows of opportunities for me and helped me meet new people like Kevin, Ms.Delgado, Mike, and Carol. I really hope we cross paths again professionally and work together once again. I know it doesn't sound like you saved me because all you did was get me a job, but in my eyes you did much more than that.”",
+      },
+      {
+        title: "Town of Hempstead Department of Occupational Resources",
+        image: "Monet Williams - Elizabeth Ajasin",
+        copy: `The year 2020 posed many challenges for everyone young and old. The summer youth employment program funded by the Town of Hempstead Department of Occupational Resources at the Five Towns Community Center, was no exception. As we began to plan what the summer might look like for our youth workers, we had the wonderful opportunity to support our pantry that due to COVID-19 had become central to our efforts in assisting families in need. Prior to this global pandemic, Gammys Pantry at the Five Towns Community Center had been slowly growing. Thanks to partnerships with Long Island Cares and other organizations/donations, the pantry grew exponentially, which in turn required more volunteer support. 
+
+        Typically youth workers are placed in sites such as our Summer Day Camp, maintenance, Headstart, and clerical settings but due to building capacity restrictions, those sites were unavailable. Gammy’s Pantry at the Five Towns Community Center turned out to be a great worksite for our youth. Youth engaged in in-person work experience participated in activities such as:  sorting donations received, set up/breakdown of distribution events, distribution of food items, maintenance of the onsite community garden, organization of the pantry, record keeping for the pantry.  Throughout their participation, youth acquired valuable organizational skills, communication skills, teambuilding, customer service skills, etc.
+        
+        Youth workers reported having had an “amazing experience” as they helped to organize and participate in distribution events (clothing and food), and “felt as if they were really helping others".  The summer program this year at the Five Towns Community Center definitely had a different feel to it. Not only did youth acquire transferable work skills but also developed a deeper connection to their community by way of their service. We are thankful that although COVID-19 created many challenges, that our youth were able to have this experience.`,
+      },
+    ],
   },
   {
     region: "Mohawk Valley",
