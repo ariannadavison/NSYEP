e67e54d65307a96ebd68cab30598b0eb0ccc87f7
Arianna Davison <arianna.davison@gmail.com>
2021-07-26 14:16:27 -0400

    drowdown arrows

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Home from "./components/Home";
 import Region from "./components/Region";
 // import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import Error404 from './components/Error404'
 // import IndividualStories from "./components/IndividualStories/IndividualStories";
 import Header from "./components/Header/Header";
 import MapV2 from './components/MapV2/MapV2';
-import RegionalMapV2 from "./components/RegionalMapV2"
+import RegionalMapV2 from "./components/RegionalMap/RegionalMapV2"
 import RegionalMap from "./components/RegionalMap/RegionalMap";
 //*Structure for Region Page:
 // <logoHero />
@@ -46,10 +47,11 @@ function App() {
           {/* fun react-router knowledge fact: this long path has to go first because its path is more precise */}
           <Route path='/regionalmap' component={RegionalMapV2} />
           {/* <Route path="/:regionName/:storyName" component={IndividualStories} /> */}
-          <Route path="/:regionName" component={Region} />
+          <Route path="/:regionName" exact component={Region} />
 
           {/* note from myself to remember how to pass props to a component here. this is code from a work project */}
           {/* <Route path="/users" render={() => <UserManagement currentUser={this.state.currentUser} />} /> */}
+          <Route component={Error404} />
         </Switch>
         <Footer />
       </div>
---
+++ src/components/Error404.js
@@ -0,0 +1,6 @@
+const Error404 = () => {
+    return (
+        "error 404"
+    )
+}
+export default Error404
