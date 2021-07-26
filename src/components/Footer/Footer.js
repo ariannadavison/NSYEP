2516d4fb8dea728d8da7b2420eaf97b5191ec14a
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-07-25 22:54:43 -0400

    footer compressing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "./footer2.css";
+import "./footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__columns">
         <div className="footer__info">
-          <p>NYATEP</p>
-          <p>P.O. Box 648</p>
-          <p>Albany, New York 12201</p>
-          <p>518-728-9057</p>
+          <p><b>NYATEP</b><br />
+          518-728-9057</p>
+          <p>P.O. Box 648 <br />
+          Albany, New York 12201</p>
+          {/* <p>518-728-9057</p> */}
         </div>
         <div className="footer__social">
           <div className="twitter social-placeholder"></div>
