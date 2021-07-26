29960fe5bfee2ff1504bc8adb3bca636f0434a3c
Arianna Davison <arianna.davison@gmail.com>
2021-07-26 14:12:06 -0400

    drowdown arrows

--- src/components/Region.js
+++ src/components/Region.js
@@ -1,19 +1,42 @@
 import RegionBanner from "./RegionBanners/RegionBanner";
 // import YearlyInfo from "./YearlyInfo/YearlyInfo";
-
+import Regions, { regions } from '../data/Regions'
+import Error404 from "./Error404";
 export default function Region(props) {
   console.log(props.match.params.regionName);
   let name = props.match.params.regionName;
   // the name comes through as the url, so we must name-ify it.
+  const regionArray = regions.map((region) => {
+    return region.region.toLowerCase()
+  })
+  console.log(regionArray)
   name = name.replace(/-/g, ' ');
   name = name.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase());
+  const getRegion = () => {
+
+    let output = ""
+    if (regionArray.indexOf(name.toLowerCase()) == -1) {
+      output = <Error404 />
+
+    }
+    else {
+      output =
+        <div className="margins">
+          <RegionBanner region={name} />
+          i am a region.<br />
+          specifically, the region known as {name}.
+        </div>
+
+    }
+    return output
+
+
+  }
 
   return (
     <div>
-      <RegionBanner region={name} />
       <div className="margins">
-        i am a region.<br />
-        specifically, the region known as {name}.
+        {getRegion()}
       </div>
       {/* <YearlyInfo regionName={name} /> */}
     </div>
