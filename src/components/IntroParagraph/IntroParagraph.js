618b8fa740a135efb7e7f77457be119447a826d4
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-08-03 00:45:11 -0400

    using container classes and column classes to size responsively

--- src/components/IntroParagraph/IntroParagraph.js
+++ src/components/IntroParagraph/IntroParagraph.js
@@ -3,7 +3,7 @@ import "./introParagraph.css";
 
 const IntroParagraph = () => {
   return (
-    <div className="paragraph margins">
+    <div className="paragraph">
       <p>In partnership with young adult alumni from Per Scholas, NYATEP has developed this dynamic website to showcase the impact and importance of summer work experience across the State of New York.</p>
     </div>
   )
