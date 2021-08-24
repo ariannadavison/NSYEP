2510e332a0af7d268f1eeae92555a552ffb5ebd2
Amelia Zecker <47397924+ameliadz@users.noreply.github.com>
2021-08-24 19:39:56 -0400

    alignment changes for small breakpoints

--- src/components/WhatIs/WhatIs.js
+++ src/components/WhatIs/WhatIs.js
@@ -6,12 +6,12 @@ const WhatIs = () => {
   return (
     <div className="what">
       <img src={picture} className="what__picture" />
-      <div>
-        <h2 className="what__header">What is SYEP?</h2>
+      <div class="what__header">
+        <h2 className="what__header-text">What is SYEP?</h2>
         <div className="what__header-bg"></div>
-        <p>The Summer Youth Employment Program (SYEP) supports communities across the state in creating summer jobs for youth from low-income families. These summer work opportunities allow young New Yorkers to gain new skills this summer, resulting in improved academic performance and increasing future job prospects.</p>
-        <p>To be eligible, young people must be ages 14 to 20 and have a family income below 200 percent of the federal poverty level (or $39,850 for a family of three). Employers can use the funds to subsidize wages, support education and training activities, as well as offer counseling and employment-related services, such as transportation to and from work.</p>
       </div>
+      <p>The Summer Youth Employment Program (SYEP) supports communities across the state in creating summer jobs for youth from low-income families. These summer work opportunities allow young New Yorkers to gain new skills this summer, resulting in improved academic performance and increasing future job prospects.</p>
+      <p>To be eligible, young people must be ages 14 to 20 and have a family income below 200 percent of the federal poverty level (or $39,850 for a family of three). Employers can use the funds to subsidize wages, support education and training activities, as well as offer counseling and employment-related services, such as transportation to and from work.</p>
     </div>
   )
 }
