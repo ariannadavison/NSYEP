d733172d0445453e66969ac5057ead937f67fa2d
Amelia Zecker <47397924+ameliadz@users.noreply.github.com>
2021-08-24 23:55:06 -0400

    alt text and spacing tweaks

--- src/components/WhatIs/WhatIs.js
+++ src/components/WhatIs/WhatIs.js
@@ -10,7 +10,7 @@ const WhatIs = () => {
         <div className="what__header-bg"></div>
       </div>
 
-      <img src={picture} className="what__picture my-2" />
+      <img src={picture} className="what__picture my-2" alt="Young man in a workshop" />
 
       <p>The Summer Youth Employment Program (SYEP) supports communities across the state in creating summer jobs for youth from low-income families. These summer work opportunities allow young New Yorkers to gain new skills this summer, resulting in improved academic performance and increasing future job prospects.</p>
       <p>To be eligible, young people must be ages 14 to 20 and have a family income below 200 percent of the federal poverty level (or $39,850 for a family of three). Employers can use the funds to subsidize wages, support education and training activities, as well as offer counseling and employment-related services, such as transportation to and from work.</p>
--- src/components/WhySYEP/WhySYEP.js
+++ src/components/WhySYEP/WhySYEP.js
@@ -9,7 +9,7 @@ const WhySYEP = () => {
         <h2 className="why__header-text">Why does it matter?</h2>
         <div className="why__header-bg"></div>
       </div>
-      <img src={picture} className="why__picture my-2" />
+      <img src={picture} className="why__picture my-2" alt="Two people show off farm-grown squash" />
       <p>Prior to COVID-19 the youth unemployment rate in New York State hovered around 20.7% for ages 16 to 19 and 11.6% for ages 20 to 24. Additionally, the NYC Comptroller recently highlighted the alarming rise in the unemployment rate among young people, ages 16 to 24 in New York City to 35.2% from its pre-pandemic rate of 6.6%. High unemployment for young people further exacerbates growing economic inequality. Earnings gained by young adults can support the critical economic needs of their families and create more financially sustainable homes.</p>
       <p>SYEP is the entry level pathway of economic mobility for New York's most vulnerable youth and their families. The program exposes young adults to the world of work and grants young adults the opportunity to develop critical “soft skills” such as working in teams, showing up on time, problem solving, and customer service that are crucial to long-term employability. A critical set of skills many employers see lacking in today's workforce.</p>
     </div>
