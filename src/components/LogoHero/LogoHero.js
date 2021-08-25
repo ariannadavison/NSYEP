833b9b7c71e6a173321fbe05609ac39d25ec9d25
Amelia Zecker <47397924+ameliadz@users.noreply.github.com>
2021-08-25 00:20:53 -0400

    simpler header style

--- src/components/LogoHero/LogoHero.js
+++ src/components/LogoHero/LogoHero.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./logoHero.css";
-import banner from "./NYATEPBanner.png";
+// import banner from "./NYATEPBanner.png";
+import banner from "./Banner.png";
 
 const LogoHero = () => {
   return (
     <div className="logoHero__container">
-      <img src={banner} />
+      <img src={banner} className="mb-2"/>
     </div>
   );
 };
