91a0f8030aeba28b255ac5dbef8882d175c56acd
Amelia Zecker <47397924+ameliadz@users.noreply.github.com>
2021-08-24 20:10:46 -0400

    responsive homepage sections

--- src/components/MottoBanner/MottoBanner.js
+++ src/components/MottoBanner/MottoBanner.js
@@ -5,11 +5,11 @@ const MottoBanner = () => {
   return (
     <div className="motto__banner">
       <div className="motto__text mb-3">
-        <span>Summer Youth</span><br />
+        <p className="mb-0">Summer Youth</p>
         <div className="motto__bg"></div>
-        <span>Employment</span><br />
+        <p className="mb-0">Employment</p>
         <div className="motto__bg"></div>
-        <span>Matters</span>
+        <p className="mb-0">Matters</p>
         <div className="motto__bg"></div>
       </div>
       <div className="banner__decorativeLine"></div>
