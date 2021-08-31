de93bafebf7148018d9ea312ea7ad230bfd147b3
Jessica Sanchez <66699748+honeyai@users.noreply.github.com>
2021-08-30 20:15:21 -0400

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,15 @@
+import './AboutUs.css'
+
+const Members = () => {
+    return (
+        <div className="about__section">
+            <h2 className="about__header">Our Members</h2>
+            <div className="about__header-bg"></div>
+            <p>
+                NYATEP’s primary audience are dues-paying members and their partners, which include the State’s 33 local workforce development boards who oversee and coordinate over $300M of federal workforce funding, the State and City community college systems, literacy providers, community-based organizations, local governments, labor unions, economic development agencies, and career and technical education providers, among others. Collectively, our membership through their educational, employment training and social supportive services impact the lives of over one million New Yorkers who are predominantly Black, Indigenous, and People of Color.
+            </p>
+        </div>
+    )
+}
+
+export default Members
